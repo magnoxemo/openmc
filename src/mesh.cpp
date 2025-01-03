c559c19f45414c0afed139830f8d0b1826fa1099
magnoxemo <walid.ahammed01@gmail.com>
2025-01-03 11:56:01 -0600

    fixing bugs

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -3179,21 +3179,23 @@ void LibMesh::set_score_data(const std::string& var_name,
 
   for (auto it = m_->local_elements_begin(); it != m_->local_elements_end(); it++) {
 
-  if (it->refinement_flag()==Elem::AMALGAMATE && ((*it)->active())){
-    for (unsigned_int side = 0; side < it->n_sides(); ++side) {
-      const libMesh::Elem* neighbor = it->neighbor_ptr(side);
-      auto bin = get_bin_from_element(*_neighbour);
-      // set value
-      vector<libMesh::dof_id_type> value_dof_indices;
-      dof_map.dof_indices(*it, value_dof_indices, value_num);
-      Ensures(value_dof_indices.size() == 1);
-      eqn_sys.solution->set(value_dof_indices[0], values.at(bin));
+  if (*it->refinement_flag()==Elem::AMALGAMATE && ((*it)->active())){
+    for (unsigned int side = 0; side < *it->n_sides(); ++side) {
+      const libMesh::Elem* _neighbor = *it->neighbor_ptr(side);
+      if (_neighbor) {
+        auto bin = get_bin_from_element(*_neighbour);
+        // set value
+        vector<libMesh::dof_id_type> value_dof_indices;
+        dof_map.dof_indices(*it, value_dof_indices, value_num);
+        Ensures(value_dof_indices.size() == 1);
+        eqn_sys.solution->set(value_dof_indices[0], values.at(bin));
 
-      // set std dev
-      vector<libMesh::dof_id_type> std_dev_dof_indices;E
-      dof_map.dof_indices(*it, std_dev_dof_indices, std_dev_num);
-      Ensures(std_dev_dof_indices.size() == 1);
-      eqn_sys.solution->set(std_dev_dof_indices[0], std_dev.at(bin));
+        // set std dev
+        vector<libMesh::dof_id_type> std_dev_dof_indices;E
+        dof_map.dof_indices(*it, std_dev_dof_indices, std_dev_num);
+        Ensures(std_dev_dof_indices.size() == 1);
+        eqn_sys.solution->set(std_dev_dof_indices[0], std_dev.at(bin));
+      }
     }
   }
 
